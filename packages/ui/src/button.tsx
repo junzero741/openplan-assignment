62c8f034b83866f6903a8ee186ee003c62e3c312
준영 <junzero741@gmail.com>
2026-02-11 20:39:17 +0900

    fix: 버튼 클릭이 되지 않는 문제 해결

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -5,11 +5,12 @@ import { twMerge } from "tailwind-merge";
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
-export const Button = ({ children, className }: ButtonProps) => {
+export const Button = ({ children, className, ...rest }: ButtonProps) => {
   return (
     <button
       className={twMerge(`rounded-xl font-semibold leading-6 p-3 bg-[#111111] text-white hover:opacity-80 active:opacity-80`,
         className)}
+      {...rest}
     >
       {children}
     </button>
