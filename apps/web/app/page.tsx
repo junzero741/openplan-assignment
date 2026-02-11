a112f23e3753e0bd56e3498667a4e8c3cd8dab52
준영 <junzero741@gmail.com>
2026-02-11 19:13:08 +0900

    feat: 메인 페이지 요소 정렬

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,8 +2,14 @@ import Header from "@/components/header"
 
 export default function Home() {
   return (
-    <div>
-      <main>
+    <div className="flex flex-col min-h-screen">
+      <Header className="text-[#1A1A1A]" />
+      <main className="flex flex-col flex-grow h-full justify-center">
+        <div className="flex justify-center items-center">
+          <p className="whitespace-pre-wrap text-center text-2xl font-semibold">
+            {`안녕하세요\n정준영입니다.`}
+          </p>
+        </div>
       </main>
     </div>
   );
