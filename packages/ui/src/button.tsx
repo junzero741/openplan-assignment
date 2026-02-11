c46097f727314ff4f52cda96f5b58677287c3ed7
준영 <junzero741@gmail.com>
2026-02-11 19:30:13 +0900

    ui: Button 의 prop이 기본적으로 button 태그를 상속하도록 수정

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
-export interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className }: ButtonProps) => {
   return (
     <button
       className={twMerge(`rounded-xl font-semibold leading-6 p-3 bg-[#111111] text-white hover:opacity-80 active:opacity-80`,
