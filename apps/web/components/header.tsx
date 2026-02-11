323669a6d60df752a879b98e9ddd9c80dc323979
준영 <junzero741@gmail.com>
2026-02-11 18:35:36 +0900

    feat: 공용 헤더 컴포넌트 작성

---
+++ apps/web/components/header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export interface HeaderProps extends React.HTMLAttributes<HTMLHeadElement> {
+
+};
+
+export default function Header({
+    className = "",
+}: HeaderProps) {
+    return (
+        <header
+            className={`w-full bg-transparent px-5 py-4 text-white ${className}`}
+            aria-label="Site header"
+        >
+            <div className="flex justify-center leading-[1.4]">정준영</div>
+        </header>
+    );
+}
