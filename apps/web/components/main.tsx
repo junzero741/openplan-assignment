e7fb9c1f262d4b3b747ff2ee58d5a0e55acf0795
준영 <junzero741@gmail.com>
2026-02-11 22:23:01 +0900

    feat: 공용 메인 레이아웃 컴포넌트 작성

---
+++ apps/web/components/main.tsx
@@ -0,0 +1,16 @@
+import { twMerge } from "tailwind-merge"
+
+interface MainProps extends React.HTMLAttributes<HTMLElement> {
+
+}
+
+
+const Main = ({ className, children, ...rest }: MainProps) => {
+    return (
+        <main className={twMerge("flex flex-col flex-grow justify-center", className)} {...rest}>
+            {children}
+        </main>
+    )
+}
+
+export default Main;
