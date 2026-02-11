08579514d30131b34d7e87e0521eee5567879ab9
준영 <junzero741@gmail.com>
2026-02-12 01:26:09 +0900

    feat: 사진 조회 정보가 새로고침시에도 남아있도록 수정

--- apps/web/stores/photoStore.ts
+++ apps/web/stores/photoStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export type PhotoInfo = {
     id: string;
@@ -14,7 +15,15 @@ type PhotoState = {
     setPhoto: (photo: PhotoInfo) => void;
 };
 
-export const usePhotoStore = create<PhotoState>((set) => ({
-    photo: null,
-    setPhoto: (photo) => set({ photo }),
-}));
+export const usePhotoStore = create<PhotoState>()(
+    persist(
+        (set) => ({
+            photo: null,
+            setPhoto: (photo) => set({ photo }),
+        }),
+        {
+            name: "photo-store",
+            partialize: (state) => ({ photo: state.photo }),
+        }
+    )
+);
