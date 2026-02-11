dfd1773081788b82baae2dd68e8d6c2088cb95e0
준영 <junzero741@gmail.com>
2026-02-11 22:25:06 +0900

    feat: 공용 푸터 레이아웃 컴포넌트 작성

---
+++ apps/web/components/footer.tsx
@@ -0,0 +1,16 @@
+import { twMerge } from "tailwind-merge"
+
+interface FooterProps extends React.HTMLAttributes<HTMLElement> {
+
+}
+
+
+const Footer = ({ className, children, ...rest }: FooterProps) => {
+    return (
+        <footer className={twMerge("px-5 py-10", className)} {...rest}>
+            {children}
+        </footer>
+    )
+}
+
+export default Footer;
