7b4da86eb946598d146e35a01a1e1b64b045574d
준영 <junzero741@gmail.com>
2026-02-11 21:51:52 +0900

    feat: result 페이지 선언

---
+++ apps/web/app/result/page.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/header"
+import PageContainer from "@/components/pageContainer";
+
+export default function Result() {
+  return (
+    <PageContainer>
+      <Header />
+      <main className="flex flex-col flex-grow justify-center">
+        <section className="flex justify-center items-center flex-grow">
+
+        </section>
+        <footer className="px-5 py-10">
+
+        </footer>
+      </main>
+    </PageContainer>
+  );
+}
