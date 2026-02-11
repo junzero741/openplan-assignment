7dd53daf58f68ac0bc46dbc36fe1250303b2b281
준영 <junzero741@gmail.com>
2026-02-11 20:41:42 +0900

    feat: 메인 페이지에서 다음 버튼 클릭 시 사진 조회 후 result 페이지로 이동

--- apps/web/components/photoLinkButton.tsx
+++ apps/web/components/photoLinkButton.tsx
@@ -1,16 +1,38 @@
-import { Button, ButtonProps } from "@repo/ui/button";
-import Link, { LinkProps } from "next/link";
-import React from "react";
+'use client';
 
-export interface PhotoLinkButtonProps extends LinkProps {
-    children?: React.ReactNode;
+import { usePhotoInfoQuery } from "@/actions/usePhotoInfoQuery";
+import { usePhotoStore } from "@/stores/photoStore";
+import { Button, ButtonProps } from "@repo/ui/button";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+export interface PhotoLinkButtonProps extends ButtonProps {
+    href: string | URL;
 }
 
 const PhotoLinkButton = ({ href, children }: PhotoLinkButtonProps) => {
+    const { refetch } = usePhotoInfoQuery({ enabled: false });
+    const photo = usePhotoStore((state) => state.photo);
+    const router = useRouter();
+
+    const handleClick = async () => {
+        if (!photo) {
+            await refetch();
+        }
+        router.push(String(href));
+    };
+
+    // 사진을 한번이라도 조회한 이력이 있을 시 "/result" 페이지로 자동으로 이동
+    useEffect(() => {
+        if (photo) {
+            router.push(String(href));
+        }
+    }, [photo]);
+
     return (
-        <Link href={href}>
-            <Button type="button" className="w-full">{children}</Button>
-        </Link>
+        <Button type="button" className="w-full" onClick={handleClick}>
+            {children}
+        </Button>
     );
 };
 
